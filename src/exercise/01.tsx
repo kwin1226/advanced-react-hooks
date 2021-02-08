e638f9f8be2236b2710be82f7bbd7114ceb1232c
Andrew <shenhokuan@gmail.com>
2021-02-09 00:20:23 +0800

    01-extra credit-03

--- src/exercise/01.tsx
+++ src/exercise/01.tsx
@@ -3,13 +3,15 @@
 
 import * as React from 'react'
 
-const countReducer = (prevState, nextState) => nextState
+const countReducer = (prevState, action) =>
+  typeof action === 'function' ? action(prevState) : action
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
 
   return <button onClick={increment}>{count}</button>
 }
