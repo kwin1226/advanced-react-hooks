6ba430922e29ee9887c53244bf95e5ae8086afbc
Andrew <shenhokuan@gmail.com>
2021-02-08 23:11:23 +0800

    01-replace useState with useReducer

--- src/exercise/01.tsx
+++ src/exercise/01.tsx
@@ -3,16 +3,22 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
+const countReducer = (prevState, action) => ({
+  count: action.payload.count,
+})
+
+function Counter({initialState = {count: 0}, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
-
+  const [state, dispatch] = React.useReducer(countReducer, initialState)
+  const {count} = state
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+
+  const increment = () =>
+    dispatch({type: 'increase', payload: {count: count + step}})
   return <button onClick={increment}>{count}</button>
 }
 
